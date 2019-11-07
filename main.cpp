a298d61ee2c0b40b02946e21aa5a1391d7319c93
sparkolo <albertogiudice@hotmail.it>
2019-11-07 13:46:19 +0100

    CMakeLists created with VLD leak detection support

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*  
+ * Created by Alberto Giudice on 07/11/2019.
+ * LIST OF EDITS (reverse chronological order - add last on top):
+ * + 
+ * + Alberto Giudice [07/11/19] - vld implementation
+ */
+
+#include <vld.h>
+
+int main(int argc, char** argv) {
+	// Create the game here
+
+    return 0;
+}
